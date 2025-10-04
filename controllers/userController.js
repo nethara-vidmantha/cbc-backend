ffba1668c8653e1ce843c089d610c88837d7d260
Nethara <netharavidmantha54@gmail.com>
2025-10-05 03:32:40 +0530

    project initialized with user login and product managing features. ✏️

---
+++ controllers/userController.js
@@ -0,0 +1,116 @@
+import User from "../models/user.js"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+export function createUser(req,res){
+
+        const hashPassword = bcrypt.hashSync(req.body.password,10)  //hash 10times
+
+        const user = new User(
+            {
+                email :req.body.email,
+                firstName :req.body.firstName,
+                lastName :req.body.lastName,
+                password : hashPassword
+            } 
+        )
+        user.save().then(
+            ()=>{
+                res.json(
+                    {
+                        Message:"User created succecsfully"
+                    }
+                )
+            }
+        ).catch(
+            ()=>{
+                res.json(
+                    {
+                        Message:"User creation failed"
+                    }
+                )
+            }
+        )
+}
+
+export function loginUser(req,res){
+
+    User.findOne(         //email ekt samana user kenek innwd kyl balnw
+        {
+            email : req.body.email
+        }
+    ).then(
+        (user)=>{
+            if(user == null){   //user kenek na
+                res.json(
+                    {
+                        message:"User not found"
+                    }
+                )
+            }
+            else{
+                const isPasswordMatching = bcrypt.compareSync(req.body.password,user.password) 
+                //user.password kynne dB eke tyena pwd eka
+
+                if(isPasswordMatching){
+
+                    const token = jwt.sign(
+                        {
+                            email: user.email,
+                            firstName: user.firstName,
+                            lastName: user.lastName,
+                            role: user.role,
+                            isEmailVerified: user.isEmailVerified
+                        },
+                        "jwt-secret"  //this is the key
+                    )
+
+                    res.json(
+                        {
+                            message:"Login successfull",
+                            token:token
+                        }
+                    )
+                }else{
+                    res.status(401).json({
+                        message:"Invalid password"
+                    })
+                }
+            }
+        }
+    )
+
+}
+
+
+export function getUser(req,res){
+        User.find().then(
+            (users)=>{
+                res.json(
+                    users
+                )
+            }
+        ).catch(
+            ()=>{
+
+            }
+        )     
+}
+
+export function deleteUser(req,res){
+        console.log("Delete request received")    
+}
+
+export function updateUser(req,res){
+        console.log("Put request received")    
+}
+
+export function isAdmin(req){
+    if(req.user == null){
+        return false;
+    }
+    if(req.user.role != "admin"){
+        return;
+    }
+    return true;
+}
---
+++ index.js
@@ -0,0 +1,56 @@
+import express from "express";
+import mongoose from "mongoose";
+import userRouter from "./routes/userRouter.js";
+import jwt from "jsonwebtoken";
+import productRouter from "./routes/productRouter.js";
+
+const app = express();
+
+app.use(express.json()) //act like middle man
+app.use(
+    (req,res,next)=>{
+        let token = req.header("Authorization")
+        if(token!= null){
+            token =token.replace("Bearer ","")
+            
+
+            
+            jwt.verify(token,"jwt-secret",      //decrypt the token
+                (err,decoded)=>{    
+                    if(decoded== null){
+                        res.json({
+                            message:"Invalid token please login agin"
+                        })
+                        return
+                    }else{
+                        req.user = decoded  //meya ilagat ywn nisa userge details tika ywnw
+                    }
+                }
+            )  
+        }
+        next()
+    }
+)
+
+
+
+const connectionString="mongodb+srv://admin:123@cluster0.sfeetsa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+mongoose.connect(connectionString).then( 
+    ()=>{
+        console.log("Database connected")
+    }
+).catch(
+    ()=>{
+        console.log("Database connection failed")
+    }
+) 
+
+app.use("/users",userRouter) 
+app.use("/products",productRouter)
+
+app.listen(5000,
+    ()=>{
+    console.log("Server is running on port 5000");    
+    }
+);
