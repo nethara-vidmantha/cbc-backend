f6bf771f16771ea5a59e56506c7a85e9ffb67319
Nethara Vidmantha <xnethara@gmail.com>
2025-10-11 14:02:40 +0530

    created .env file and added the  db url 📢

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ import mongoose from "mongoose";
 import userRouter from "./routes/userRouter.js";
 import jwt from "jsonwebtoken";
 import productRouter from "./routes/productRouter.js";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+app.use(cors())
 
 app.use(express.json()) //act like middle man
 app.use(
@@ -34,7 +40,7 @@ app.use(
 
 
 
-const connectionString="mongodb+srv://admin:123@cluster0.sfeetsa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const connectionString = process.env.MONGO_URI;
 
 mongoose.connect(connectionString).then( 
     ()=>{
